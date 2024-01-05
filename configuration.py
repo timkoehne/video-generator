7fdbb7041fd92cd5d268373d7043fc122104df6f
Tim Köhne <20153452+timkoehne@users.noreply.github.com>
2024-01-05 22:45:12 +0100

    finished text thumbnail generation

--- configuration.py
+++ configuration.py
@@ -6,6 +6,9 @@ class Configuration:
     def __init__(self) -> None:
         with open("config/configuration.json") as file:
             config = json.loads(file.read())
+            
+        with open("config/config_thumbnail_with_text.json") as file:
+            config_thumbnail_with_text = json.loads(file.read())
 
         self.output_dir = config["output_dir"]
         self.background_videos_dir = config["background_videos_dir"]
@@ -16,18 +19,53 @@ class Configuration:
         self.video_title_prompt: str = config["video_title_prompt"]
         self.background_video_volume: float = config["background_video_volume"]
         self.thumbnail_image_dir: str = config["thumbnail_image_dir"]
-        self.thumbnail_text_width_percent: float = config["thumbnail_text_width_percent"]
+        self.thumbnail_text_width_percent: float = config[
+            "thumbnail_text_width_percent"
+        ]
         self.thumbnail_allowed_overlap: float = config["thumbnail_allowed_overlap"]
         self.thumbnail_edge_width: int = config["thumbnail_edge_width"]
-        self.thumbnail_person_aspect_ratio_min: float = config["thumbnail_person_aspect_ratio_min"]
-        self.thumbnail_person_aspect_ratio_max: float = config["thumbnail_person_aspect_ratio_max"]
-        
+        self.thumbnail_person_aspect_ratio_min: float = config[
+            "thumbnail_person_aspect_ratio_min"
+        ]
+        self.thumbnail_person_aspect_ratio_max: float = config[
+            "thumbnail_person_aspect_ratio_max"
+        ]
 
         self.init_text_clips(config)
         self.init_text_wall(config)
 
         self.init_openai(config)
         self.init_moviepy(config)
+
+        self.init_thumbnail(config_thumbnail_with_text)
+
+    def init_thumbnail(self, config):
+        self.thumbnail_subreddit_icon_size: int = config[
+            "thumbnail_subreddit_icon_size"
+        ]
+        self.thumbnail_subreddit_text_size: int = config[
+            "thumbnail_subreddit_text_size"
+        ]
+        self.thumbnail_subreddit_text_color: str = config[
+            "thumbnail_subreddit_text_color"
+        ]
+        self.thumbnail_subreddit_text_font: str = config[
+            "thumbnail_subreddit_text_font"
+        ]
+        self.thumbnail_main_text_size: int = config["thumbnail_main_text_size"]
+        self.thumbnail_main_text_color: str = config["thumbnail_main_text_color"]
+        self.thumbnail_main_text_font: str = config["thumbnail_main_text_font"]
+        self.thumbnail_like_icon_size: int = config["thumbnail_like_icon_size"]
+        self.thumbnail_like_text_size: int = config["thumbnail_like_text_size"]
+        self.thumbnail_like_text_color: str = config["thumbnail_like_text_color"]
+        self.thumbnail_like_text_font: str = config["thumbnail_like_text_font"]
+        self.thumbnail_element_margin: int = config["thumbnail_element_margin"]
+        self.thumbnail_background_color: str = config["thumbnail_background_color"]
+        self.thumbnail_side_offset: float = config["thumbnail_side_offset"]
+        self.thumbnail_default_subreddit_icon: str = config["thumbnail_default_subreddit_icon"]
+        self.thumbnail_upvotes_icon: str = config["thumbnail_upvotes_icon"]
+        self.thumbnail_comments_icon: str = config["thumbnail_comments_icon"]
+        
 
     def init_text_wall(self, config):
         self.text_wall_font: str = config["text_wall_font"]
--- thumbnail_with_text.py
+++ thumbnail_with_text.py
@@ -1,119 +1,167 @@
+import sys
 from typing import Tuple
 from moviepy import CompositeVideoClip, ImageClip, TextClip, VideoClip
 from moviepy.video.fx import resize
 import numpy as np
+from configuration import Configuration
 from reddit_requests import Post, create_post_from_post_id
 from PIL import Image
 import urllib.request
 
-
-sub_reddit_icon_size = 200
-bottom_row_margin = 20
-bottom_row_image_size = 250
-background_color = (255, 255, 255)
+config = Configuration()
 
 
-
-def get_concat_v_blank(im1, im2, color=(0, 0, 0)):
-    dst = Image.new("RGB", (max(im1.width, im2.width), im1.height + im2.height), color)
-    dst.paste(im1, (0, 0))
-    dst.paste(im2, (0, im1.height))
-    return dst
+def hex_to_rgb(hex: str) -> Tuple[int, int, int]:
+    if hex.startswith("#"):
+        hex = hex[1:]
+    return tuple(int(hex[i : i + 2], 16) for i in (0, 2, 4))  # type: ignore
 
 
-def generate_subreddit_part(
-    post: Post, background_color: Tuple[int, int, int]
+def change_image_color(
+    img: Image.Image, new_color: Tuple[int, int, int]
 ) -> Image.Image:
-    subreddit_image: Image.Image
-    with urllib.request.urlopen(post.subreddit_icon_url) as url:
-        subreddit_image = Image.open(url)
-        if subreddit_image.mode != "RGBA":
-            subreddit_image = subreddit_image.convert("RGBA")
-    
-    
+    image = np.array(img)
+
+    rgb = image[..., :3]
+    alpha = image[..., 3]
+
+    non_transparent_pixels = alpha != 0
+    # new_color = np.array(new_color)
+
+    rgb[non_transparent_pixels] = new_color
+    modified_image_array = np.concatenate((rgb, alpha[..., np.newaxis]), axis=-1)
+    modified_image = Image.fromarray(modified_image_array.astype(np.uint8))
+    return modified_image
+
+
+def generate_subreddit_part(post: Post) -> Image.Image:
+    subreddit_image: Image.Image | None = None
+
+    print(f"trying to request subreddit icon")
+    for i in range(0, 5):
+        try:
+            with urllib.request.urlopen(post.subreddit_icon_url) as url:
+                subreddit_image = Image.open(url)
+                if subreddit_image.mode != "RGBA":
+                    subreddit_image = subreddit_image.convert("RGBA")
+                    subreddit_image = subreddit_image.resize(
+                        (
+                            config.thumbnail_subreddit_icon_size,
+                            config.thumbnail_subreddit_icon_size,
+                        )
+                    )
+                    break
+        except Exception:
+            print(f"retrying attempt {i}")
+
+    if subreddit_image == None:
+        print("could not find subreddit icon. using default reddit logo.")
+        subreddit_image = Image.open(config.thumbnail_default_subreddit_icon)
+
     icon_clip: ImageClip = ImageClip(np.asarray(subreddit_image))
-    resize(icon_clip, (sub_reddit_icon_size, sub_reddit_icon_size))
 
     text_clip: TextClip = TextClip(
         f"r/{post.subreddit}",
-        color="red",
-        font="Arial-Black",
-        font_size=70,
+        color=config.thumbnail_subreddit_text_color,
+        font=config.thumbnail_subreddit_text_font,
+        font_size=config.thumbnail_subreddit_text_size,
         align="west",
-        bg_color="#{:02x}{:02x}{:02x}".format(*background_color),
+        bg_color=config.thumbnail_background_color,
+        # bg_color="#{:02x}{:02x}{:02x}".format(*background_color),
     )
     text_clip = text_clip.with_position(
-        (icon_clip.size[0], (icon_clip.size[1] - text_clip.size[1]) / 2)
+        (
+            icon_clip.size[0] + config.thumbnail_element_margin,
+            (icon_clip.size[1] - text_clip.size[1]) / 2,
+        )
     )
 
     combined_clip = CompositeVideoClip(
         [icon_clip, text_clip],
-        bg_color=background_color,
-        size=(icon_clip.size[0] + text_clip.size[0], icon_clip.size[1]),
+        bg_color=hex_to_rgb(config.thumbnail_background_color),
+        size=(
+            icon_clip.size[0] + text_clip.size[0] + config.thumbnail_element_margin,
+            icon_clip.size[1],
+        ),
     )
     image = Image.fromarray(combined_clip.get_frame(0))
-
 
     return image
 
 
-def generate_text(
-    text: str, resolution: Tuple[int, int], background_color: Tuple[int, int, int]
-) -> Image.Image:
+def generate_text_part(text: str, resolution: Tuple[int, int]) -> Image.Image:
     text_clip = TextClip(
         text,
-        size=(resolution[0] * 0.8, 0),
-        color="red",
-        font="Arial-Black",
-        font_size=70,
+        size=(resolution[0] * (1 - 2 * config.thumbnail_side_offset), 0),
+        color=config.thumbnail_main_text_color,
+        font=config.thumbnail_main_text_font,
+        font_size=config.thumbnail_main_text_size,
         method="caption",
         align="west",
-        bg_color="#{:02x}{:02x}{:02x}".format(*background_color),
+        bg_color=config.thumbnail_background_color,
     )
     frame = Image.fromarray(text_clip.get_frame(0))
     return frame
 
 
-def generate_upvotes_part(background_color: Tuple[int, int, int]) -> Image.Image:
-    post = create_post_from_post_id("cvb3b6")
-
+def generate_upvotes_part(post: Post) -> Image.Image:
     positions: list[Tuple[int, int]] = []
     clips: list[VideoClip] = []
 
+    upvote_img = Image.open(config.thumbnail_upvotes_icon)
+    upvote_img = change_image_color(
+        upvote_img, hex_to_rgb(config.thumbnail_like_text_color)
+    )
+    upvote_image_arr = np.array(upvote_img)
+
+    comments_img = Image.open(config.thumbnail_comments_icon)
+    comments_img = change_image_color(
+        comments_img, hex_to_rgb(config.thumbnail_like_text_color)
+    )
+    comments_img_arr = np.array(comments_img)
+
     positions.append((0, 0))
     clips.append(
-        resize(ImageClip("config/upvote.png"), (bottom_row_image_size, bottom_row_image_size)).with_position(
-            positions[-1]
-        )
+        resize(
+            ImageClip(upvote_image_arr),
+            (config.thumbnail_like_icon_size, config.thumbnail_like_icon_size),
+        ).with_position(positions[-1])
     )
-    positions.append((positions[-1][0] + clips[-1].size[0] + bottom_row_margin, 0))
+    positions.append(
+        (positions[-1][0] + clips[-1].size[0] + config.thumbnail_element_margin, 0)
+    )
     clips.append(
         TextClip(
             str(post.upvotes),
-            color="DarkGray",
-            font="Arial-Black",
-            font_size=70,
+            color=config.thumbnail_like_text_color,
+            font=config.thumbnail_like_text_font,
+            font_size=config.thumbnail_like_text_size,
             align="center",
-            bg_color="#{:02x}{:02x}{:02x}".format(*background_color),
+            bg_color=config.thumbnail_background_color,
         ).with_position(positions[-1])
     )
 
-    positions.append((positions[-1][0] + clips[-1].size[0] + 3 * bottom_row_margin, 0))
+    positions.append(
+        (positions[-1][0] + clips[-1].size[0] + 3 * config.thumbnail_element_margin, 0)
+    )
     clips.append(
         resize(
-            ImageClip("config/speech_bubble.png"), (bottom_row_image_size, bottom_row_image_size)
+            ImageClip(comments_img_arr),
+            (config.thumbnail_like_icon_size, config.thumbnail_like_icon_size),
         ).with_position(positions[-1])
     )
 
-    positions.append((positions[-1][0] + clips[-1].size[0] + bottom_row_margin, 0))
+    positions.append(
+        (positions[-1][0] + clips[-1].size[0] + config.thumbnail_element_margin, 0)
+    )
     clips.append(
         TextClip(
             str(post.num_comments),
-            color="DarkGray",
-            font="Arial-Black",
-            font_size=70,
+            color=config.thumbnail_like_text_color,
+            font=config.thumbnail_like_text_font,
+            font_size=config.thumbnail_like_text_size,
             align="center",
-            bg_color="#{:02x}{:02x}{:02x}".format(*background_color),
+            bg_color=config.thumbnail_background_color,
         ).with_position(positions[-1])
     )
 
@@ -124,20 +172,34 @@ def generate_upvotes_part(background_color: Tuple[int, int, int]) -> Image.Image
         clips[i] = clips[i].with_position(positions[i])
     final_size = (positions[-1][0] + clips[-1].size[0], max_height)
 
-    res = CompositeVideoClip(clips, size=final_size, bg_color=background_color)
+    res = CompositeVideoClip(
+        clips, size=final_size, bg_color=hex_to_rgb(config.thumbnail_background_color)
+    )
     return Image.fromarray(res.get_frame(0))
 
 
 def generate_thumbnail_with_text(post: Post, resolution: Tuple[int, int]):
+    subreddit_part = generate_subreddit_part(post)
+    text_part = generate_text_part(post.title, resolution)
+    upvotes_part = generate_upvotes_part(post)
 
-    subreddit_part = generate_subreddit_part(post, background_color)
-    text_part = generate_text(post.title, resolution, background_color)
-    bottom_row = generate_upvotes_part(background_color)
+    image = Image.new("RGB", resolution, config.thumbnail_background_color)
 
+    text_part_pos = (
+        int(config.thumbnail_side_offset * resolution[0]),
+        int(0.5 * resolution[1] - text_part.height / 2),
+    )
+    subreddit_part_pos = (
+        int(config.thumbnail_side_offset * resolution[0]),
+        text_part_pos[1] - subreddit_part.height,
+    )
+    upvotes_part_pos = (
+        int(config.thumbnail_side_offset * resolution[0]),
+        text_part_pos[1] + text_part.height + config.thumbnail_element_margin,
+    )
 
-
-    image = get_concat_v_blank(subreddit_part, text_part, color=background_color)
-    image = get_concat_v_blank(image, bottom_row, color=background_color)
-    image.show()
-
+    image.paste(text_part, text_part_pos)
+    image.paste(subreddit_part, subreddit_part_pos)
+    image.paste(upvotes_part, upvotes_part_pos)
+    
     return image
